ab6f7774f002d3f160ccebfaa72fae3383b39cfe
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2022-10-22 18:57:03 -0300

    Estrutura base da tela de apresentação

--- src/ui/partials/index/_presentation.tsx
+++ src/ui/partials/index/_presentation.tsx
@@ -1,7 +1,16 @@
-import { SectionContainer } from "./_presentation.styled";
+import { ContainerStyled, SectionContainer } from "./_presentation.styled";
 
 const Presentation = () => {
-    return (<SectionContainer>fshjfhsjhfjfsdfdsfsdfsdfsfsfsfsfsdshjfhsjhffsdfsdfsdfsdfsdfsdfsdfsdfsdjshfhsjkd</SectionContainer>);
+    return (
+        <SectionContainer>
+            <ContainerStyled>
+                <div>Titulofhsdjfjsdhfjsjhfuiwenvjnuihenjnvnunvnvfnnvnvnn njvnndsjfsdkjfv vnjndj</div>
+                <div>Subtitulo</div>
+                <div>Botão</div>
+                <div>Imagem</div>
+            </ContainerStyled>
+        </SectionContainer>
+    );
 };
 
 export default Presentation;
