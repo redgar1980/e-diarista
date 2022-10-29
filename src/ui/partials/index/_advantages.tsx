104d1d0d083f7dd950eac0408b67b96e427227ea
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2022-10-29 19:38:52 -0300

    Construindo componente de título

--- src/ui/partials/index/_advantages.tsx
+++ src/ui/partials/index/_advantages.tsx
@@ -1,7 +1,12 @@
-import { GradientBackground } from "./_advantages.style";
+import { Container } from "@mui/material";
+import { GradientBackground, SectionTitle } from "./_advantages.style";
 
 const Advantages = () => {
-    return <GradientBackground>fdsfsdfsdjflksd</GradientBackground>;
+    return <GradientBackground>
+        <Container>
+            <SectionTitle>Por que usar o E-diaristas?</SectionTitle>
+        </Container>
+    </GradientBackground>;
 }
 
 export default Advantages;
