bcaca52f87a01d638d60fcd88e7485cc8a19731e
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-01-22 18:51:39 -0300

    Estilizando estilo padrão dos componentes no tema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import Advantages from "@partials/index/_advantages";
-import FrequestQuestion from "@partials/index/_frequent-question";
-import Presentation from "@partials/index/_presentation";
-import type { GetStaticProps, NextPage } from "next";
+import Advantages from '@partials/index/_advantages';
+import FrequestQuestion from '@partials/index/_frequent-question';
+import Presentation from '@partials/index/_presentation';
+import type { GetStaticProps, NextPage } from 'next';
 
 export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {
-            title: "",
-        },
-    };
+  return {
+    props: {
+      title: '',
+    },
+  };
 };
 
 const Index: NextPage<{ title: string }> = (props) => {
-    return (
-        <div>
-            <Presentation />
-            <Advantages />
-            <FrequestQuestion />
-        </div>
-    );
+  return (
+    <div>
+      <Presentation />
+      <Advantages />
+      <FrequestQuestion />
+    </div>
+  );
 };
 
 export default Index;
--- src/ui/themes/light-theme.ts
+++ src/ui/themes/light-theme.ts
@@ -1,53 +1,77 @@
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            light: "#9661ff",
-            main: "#6B2AEE",
-            dark: "#581ECD",
-        },
-        secondary: {
-            light: "#2bfff2",
-            main: "#02E7D9",
-            dark: "#1dd6cb",
-        },
-        text: {
-            primary: "#707070",
-            secondary: "#9B9B9B",
-        },
-        error: {
-            main: "#FC3C00",
-        },
-        warning: {
-            main: "#FCA600",
-        },
-        success: {
-            main: "#00D34D",
-        },
-        grey: {
-            50: "#FAFAFA",
-            100: "#F0F0F0",
-            200: "#D7D9DD",
-            300: "#C4C4C4",
-            400: "#9B9B9B",
-        },
+  palette: {
+    primary: {
+      light: '#9661ff',
+      main: '#6B2AEE',
+      dark: '#581ECD',
     },
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 960,
-            lg: 1280,
-            xl: 1536,
-        }
+    secondary: {
+      light: '#2bfff2',
+      main: '#02E7D9',
+      dark: '#1dd6cb',
     },
-    typography: {
-        fontFamily: "Poppins, Roboto, sans-serif",
+    text: {
+      primary: '#707070',
+      secondary: '#9B9B9B',
     },
-    shape: {
-        borderRadius: 3,
+    error: {
+      main: '#FC3C00',
     },
+    warning: {
+      main: '#FCA600',
+    },
+    success: {
+      main: '#00D34D',
+    },
+    grey: {
+      50: '#FAFAFA',
+      100: '#F0F0F0',
+      200: '#D7D9DD',
+      300: '#C4C4C4',
+      400: '#9B9B9B',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1536,
+    },
+  },
+  typography: {
+    fontFamily: 'Poppins, Roboto, sans-serif',
+  },
+  shape: {
+    borderRadius: 3,
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          borderWidth: '2px',
+          ':hover': {
+            borderWidth: '2px',
+          },
+        },
+      },
+      variants: [
+        {
+          props: {
+            variant: 'contained',
+            color: 'secondary',
+          },
+          style: {
+            color: 'white',
+          },
+        },
+      ],
+    },
+  },
 });
 
 export default theme;
