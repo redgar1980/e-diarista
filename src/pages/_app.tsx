69537540e5dea18ed455411c4cfa715b890b7333
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-02-02 19:42:14 -0300

    Inclusão da configuração no cypress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material'
-import theme from 'ui/themes/theme'
-import React from 'react'
-import Footer from 'ui/components/surfaces/Footer/Footer'
-import Header from 'ui/components/surfaces/Header/Header'
-import { AppContainer } from '@styles/pages/AppContainer.styled'
-import Head from 'next/head'
+import '@styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material';
+import theme from 'ui/themes/theme';
+import React from 'react';
+import Footer from 'ui/components/surfaces/Footer/Footer';
+import Header from 'ui/components/surfaces/Header/Header';
+import { AppContainer } from '@styles/pages/AppContainer.styled';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const title = `e-diarista ${pageProps.title && pageProps.title}`;
   return (
     <>
       <Head>
-        <title>e-diarista { pageProps.title && ` - ${pageProps.title}` }</title>
+        <title>{title}</title>
       </Head>
       <ThemeProvider theme={theme}>
         <AppContainer>
@@ -24,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </AppContainer>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
