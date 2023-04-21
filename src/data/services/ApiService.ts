a7e80f0a3b86e9d51d004900820c1ae70bdb432b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-04-21 20:02:52 -0300

    ESLint e Variáveis de Ambiente

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = process.env.NEXT_PUBLIC_API;
 
 export const ApiService = axios.create({
   baseURL: baseUrl,
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
   },
 });
