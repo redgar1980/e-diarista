8487db8d01e9617c66b2913a88042c1b8aac205a
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-04-23 19:00:32 -0300

    Configuracao e instalacao Editor Config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Advantages from '@partials/index/_advantages';
-import FrequestQuestion from '@partials/index/_frequent-question';
-import Presentation from '@partials/index/_presentation';
-import type { GetStaticProps, NextPage } from 'next';
+import Advantages from "@partials/index/_advantages";
+import FrequestQuestion from "@partials/index/_frequent-question";
+import Presentation from "@partials/index/_presentation";
+import type { GetStaticProps, NextPage } from "next";
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
-      title: '',
+      title: "",
     },
   };
 };
