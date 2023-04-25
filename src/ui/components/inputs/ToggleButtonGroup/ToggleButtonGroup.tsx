a92ea322297c97a0c29e76006acf06a2b8e1daf6
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-04-25 10:52:58 -0300

    Criando componente ToggleButtonGroup

---
+++ src/ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from "react";
+// import {} from '@mui/material';
+import {
+  ToggleButtonGroupStyled,
+  ToggleButtonStyled,
+} from "./ToggleButtonGroup.styled";
+
+export interface ToggleButtonGroupProps {}
+
+const ToggleButtonGroup = ToggleButtonGroupStyled;
+export const ToggleButton = ToggleButtonStyled;
+
+export default ToggleButtonGroup;
