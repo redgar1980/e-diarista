bf515115c1054862095cd70e1e494f1ead2e5a5b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-04 20:36:12 -0300

    Iniciando formulário de contratação

--- src/pages/encontrar-diarista.tsx
+++ src/pages/encontrar-diarista.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { GetStaticProps } from 'next';
-import VerificarProfissionais from '@partials/encontrar-diarista/_verificar-profissionais';
+import React from "react";
+import { GetStaticProps } from "next";
+import VerificarProfissionais from "@partials/encontrar-diarista/_verificar-profissionais";
+import Contratacao from "@partials/encontrar-diarista/_contratacao";
 
 // import { Component } from '@styles/pages/encontrar-diarista.styled';
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
-      title: 'Encontrar Diarista',
+      title: "Encontrar Diarista",
     },
   };
 };
 
 const EncontrarDiarista: React.FC = () => {
-  return <VerificarProfissionais />;
+  //return <VerificarProfissionais />;
+  return <Contratacao />;
 };
 
 export default EncontrarDiarista;
--- src/ui/components/inputs/UserForm/UserForm.tsx
+++ src/ui/components/inputs/UserForm/UserForm.tsx
@@ -1,16 +1,23 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from "react";
 // import {} from '@mui/material';
-// import {} from './UserForm.styled';
-
+import {
+  FormContainerStyled,
+  PageFormContainerStyled,
+} from "./UserForm.styled";
 
 export interface UserFormProps {}
 
-const UserForm:React.FC<PropsWithChildren<UserFormProps>> = () => {
+export const UserFormContainer = FormContainerStyled;
+export const PageFormContainer = PageFormContainerStyled;
 
-    return (
-        <div>UserForm</div>
-    )    
-}
-
+const UserForm: React.FC<PropsWithChildren<UserFormProps>> = () => {
+  return <div>UserForm</div>;
+};
 
 export default UserForm;
+
+export * from "./forms/AddressForm";
+export * from "./forms/NewContactForm";
+export * from "./forms/PaymentForm";
+export * from "./forms/PictureForm";
+export * from "./forms/UserDataForm";
