b0ffcfc9e0a356e96aa5cfb7c489859c4f214e49
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-05 22:33:52 -0300

    Validação dos campos

--- src/data/hooks/pages/useContratacao.page.ts
+++ src/data/hooks/pages/useContratacao.page.ts
@@ -1,11 +1,17 @@
+import { FormSchemaService } from "./../../services/FormSchemaService";
 import { NovaDiariaFormDataInterface } from "data/@types/FormInterface";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export default function useContratacao() {
   const [step, setStep] = useState(1),
     breadcrumbItems = ["Detalhes da diária", "Identificação", "Pagamento"],
-    serviceForm = useForm<NovaDiariaFormDataInterface>();
+    serviceForm = useForm<NovaDiariaFormDataInterface>({
+      resolver: yupResolver(
+        FormSchemaService.address().concat(FormSchemaService.detalheServico())
+      ),
+    });
 
   return { step, breadcrumbItems };
 }
