ae71429eb4046db6388a83c096f7d39d45e4232b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-08 20:49:19 -0300

    Tamanho de Cômodos e data

--- src/ui/partials/encontrar-diarista/_detalhe-servico.tsx
+++ src/ui/partials/encontrar-diarista/_detalhe-servico.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Tooltip, Typography } from "@mui/material";
 import { ServicoInterface } from "data/@types/ServicoInterface";
 import React, { PropsWithChildren } from "react";
 import { useFormContext, Controller } from "react-hook-form";
@@ -6,19 +6,54 @@ import ToggleButtonGroup, {
   ToggleButton,
 } from "ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup";
 import { AddressForm } from "ui/components/inputs/UserForm/UserForm";
-
-// import { Component } from './_detalhe-servico.styled';
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { ItemsContainer } from "./_detalhe-servico.styled";
+import ItemCounter from "ui/components/inputs/ItemCounter/ItemCounter";
+import { FormValues } from "data/@types/forms/FormValue";
 
 interface DetalheServicoProps {
   servicos?: ServicoInterface[];
 }
+
+const houseParts = [
+  {
+    singular: "Cozinha",
+    plural: "Cozinhas",
+    nome: "quantidade_cozinhas",
+  },
+  {
+    singular: "Sala",
+    plural: "Salas",
+    nome: "quantidade_salas",
+  },
+  {
+    singular: "Banheiro",
+    plural: "Banheiros",
+    nome: "quantidade_banheiros",
+  },
+  {
+    singular: "Quarto",
+    plural: "Quartos",
+    nome: "quantidade_quartos",
+  },
+  {
+    singular: "Quintal",
+    plural: "Quintais",
+    nome: "quantidade_quintais",
+  },
+  {
+    singular: "Outros",
+    plural: "Outros",
+    nome: "quantidade_outros",
+  },
+];
 
 const DetalheServico: React.FC<DetalheServicoProps> = ({ servicos = [] }) => {
   const {
     register,
     control,
     formState: { errors },
-  } = useFormContext();
+  } = useFormContext<FormValues>();
   return (
     <div>
       <Typography sx={{ fontWeight: "bold", pb: 2 }}>
@@ -45,6 +80,93 @@ const DetalheServico: React.FC<DetalheServicoProps> = ({ servicos = [] }) => {
           </ToggleButtonGroup>
         )}
       />
+
+      <Divider sx={{ my: 5 }} />
+      <Typography sx={{ fontWeight: "bold", mb: 2 }}>
+        Qual o tamanho da sua casa?
+      </Typography>
+
+      <ItemsContainer>
+        {houseParts.map((item) => (
+          <Controller
+            key={item.nome}
+            name={`faxina.${item.nome}` as any}
+            defaultValue={0}
+            control={control}
+            render={({ field }) => (
+              <ItemCounter
+                label={item.singular}
+                plural={item.plural}
+                counter={field.value as number}
+                onInc={() => field.onChange((field.value as number) + 1)}
+                onDec={() =>
+                  field.onChange(Math.max(0, (field.value as number) - 1))
+                }
+              />
+            )}
+          />
+        ))}
+      </ItemsContainer>
+      <Divider sx={{ my: 5 }} />
+      <Typography sx={{ fontWeight: "bold", mb: 2 }}>
+        Qual a data você gostaria de receber a/o diarista?
+      </Typography>
+
+      <ItemsContainer>
+        <Controller
+          name={"faxina.data_atendimento"}
+          defaultValue={""}
+          control={control}
+          render={({ field: { ref, ...inputProps } }) => (
+            <TextFieldMask
+              {...inputProps}
+              inputRef={ref}
+              mask={"99/99/9999"}
+              label={"Data"}
+              error={errors?.faxina?.data_atendimento != undefined}
+              helperText={errors?.faxina?.data_atendimento?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name={"faxina.hora_inicio"}
+          defaultValue={""}
+          control={control}
+          render={({ field: { ref, ...inputProps } }) => (
+            <TextFieldMask
+              {...inputProps}
+              inputRef={ref}
+              mask={"99:99"}
+              label={"Hora Início"}
+              error={errors?.faxina?.hora_inicio != undefined}
+              helperText={errors?.faxina?.hora_inicio?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name={"faxina.hora_termino"}
+          defaultValue={""}
+          control={control}
+          render={({ field: { ref, ...inputProps } }) => (
+            <Tooltip title={"Campo Automático"}>
+              <div>
+                <TextFieldMask
+                  {...inputProps}
+                  inputProps={{ readOnly: true, disable: true }}
+                  inputRef={ref}
+                  mask={"99:99"}
+                  label={"Hora Término"}
+                  error={errors?.faxina?.hora_termino != undefined}
+                  helperText={errors?.faxina?.hora_termino?.message}
+                />
+              </div>
+            </Tooltip>
+          )}
+        />
+      </ItemsContainer>
+      <Divider sx={{ my: 5 }} />
       <AddressForm />
     </div>
   );
