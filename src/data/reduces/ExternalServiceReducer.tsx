ca0f6df686cbfa8d5faf8aa02219aa764cddbcd4
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-21 19:42:12 -0300

    Criando reducer

--- src/data/reduces/ExternalServiceReducer.tsx
+++ src/data/reduces/ExternalServiceReducer.tsx
@@ -1,1 +1,50 @@
+import { ApiLinksInterface } from "data/@types/ApiLinksInterface";
+import { ApiService } from "data/services/ApiService";
 import produce from "immer";
+import React, { useReducer, useEffect } from "react";
+
+export const initialState = {
+  externaService: [] as ApiLinksInterface[],
+};
+
+export type InitialStateType = typeof initialState;
+export type ExternalServiceActionType = {
+  type: string;
+  payload?: unknown;
+};
+
+const reducer = (
+  state: InitialStateType,
+  action: ExternalServiceActionType
+): InitialStateType => {
+  const nextState = produce(state, (draftState) => {
+    switch (action.type) {
+      case "UPDATE":
+        draftState.externaService = action.payload as ApiLinksInterface[];
+        break;
+    }
+  });
+  return nextState;
+};
+export interface ExternalServiceReducerInterface {
+  externalServicesState: InitialStateType;
+  externalServicesDispatch: React.Dispatch<ExternalServiceActionType>;
+}
+
+export function useExternalReducer(): ExternalServiceReducerInterface {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    ApiService.get<{ links: ApiLinksInterface[] }>("/api").then(({ data }) => {
+      dispatch({
+        type: "UPDATE",
+        payload: data.links,
+      });
+    });
+  }, []);
+
+  return {
+    externalServicesState: state,
+    externalServicesDispatch: dispatch,
+  };
+}
