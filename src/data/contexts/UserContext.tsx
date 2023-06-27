925e7a7544807ad5c8ff3eb416d5aefe5f01b9c3
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-27 20:37:16 -0300

    Pegando dados do usuário

---
+++ src/data/contexts/UserContext.tsx
@@ -0,0 +1,22 @@
+import {
+  UserReducerInterface,
+  initialState,
+  useUserReducer,
+} from "data/reduces/UserReducer";
+import { createContext } from "react";
+import React, { PropsWithChildren } from "react";
+
+const initialValue: UserReducerInterface = {
+  useDispatch: () => {},
+  useState: initialState,
+};
+
+export const UserContext = createContext(initialValue);
+
+export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const provider = useUserReducer();
+
+  return (
+    <UserContext.Provider value={provider}>{children}</UserContext.Provider>
+  );
+};
