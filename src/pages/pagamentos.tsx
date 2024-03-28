c682f46ff9bcbe9570c814b439b9e5fd5e4918a5
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-03-28 19:57:17 -0300

    Lógica da tela de pagamentos

--- src/pages/pagamentos.tsx
+++ src/pages/pagamentos.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { GetStaticProps } from 'next';
+import React from "react";
+import { GetStaticProps } from "next";
+import usePagamentos from "data/hooks/pages/usePagamentos.page";
 
 // import { Component } from '@styles/pages/pagamentos.styled';
 
 export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {
-            title: 'Pagamentos',
-        },
-    };
+  return {
+    props: {
+      title: "Pagamentos",
+    },
+  };
 };
 
 const Pagamentos: React.FC = () => {
-    return (
-        <div>
-            <div>Pagamentos</div>
-        </div>
-    );
+  const {} = usePagamentos();
+  return (
+    <div>
+      <div>Pagamentos</div>
+    </div>
+  );
 };
 
 export default Pagamentos;
